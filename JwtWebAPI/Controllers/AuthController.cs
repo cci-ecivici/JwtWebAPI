3eedf5b4a365d1de39e7f4ffc7f3bd7497d8f0cd
cci-ecivici <ecem.civici@cci.com.tr>
2023-10-19 17:42:49 +0300

    Added Role - Based Authorization.

--- JwtWebAPI/Controllers/AuthController.cs
+++ JwtWebAPI/Controllers/AuthController.cs
@@ -53,7 +53,8 @@ namespace JwtWebAPI.Controllers
         {
             List<Claim> claims = new List<Claim>()
             {
-                new Claim(ClaimTypes.Name, user.UserName)
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Role, "Admin")
             };
 
             var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
